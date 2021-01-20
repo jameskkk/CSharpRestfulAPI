170d7ff0d28cdcf57ae2862ffa78c1857b94e3b8
jameskkk <chenjhkk@gmail.com>
2021-01-20 15:44:35 +0800

    Add single app running.

--- RestFulService/Program.cs
+++ RestFulService/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.ServiceModel.Web;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -10,12 +12,27 @@ namespace RestFulService
 {
     class Program
     {
+        private static Mutex m_Mutex = null;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main(string[] args)
         {
+            ServicePointManager.DefaultConnectionLimit = 100;
+            const string appName = "CCAUBarcodeReaderApp";
+            bool createdNew;
+
+            m_Mutex = new Mutex(true, appName, out createdNew);
+
+            if (!createdNew)
+            {
+                //app is already running! Exiting the application
+                MessageBox.Show("RestFulService is already running!");
+                return;
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             // Instead of running a form, we run an ApplicationContext.
